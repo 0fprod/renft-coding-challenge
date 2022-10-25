b4bc2d8673a54281919bfac4e1c97603823bb455
franjpr <franp.dev@protonmail.com>
2022-10-25 20:09:21 +0100

    refactor: moved given to tests folder

--- src/components/NftCard/NftCard.spec.tsx
+++ src/components/NftCard/NftCard.spec.tsx
@@ -1,26 +1,11 @@
 import { screen } from '@testing-library/react'
+import { givenAnNft } from '../../../tests/given'
 import { Render } from '../../../tests/render'
-import { NFT } from '../../models/NFT'
 import { NftCard } from './NftCard'
 
 describe('NFTCard', () => {
   it('renders', () => {
-    Render(<NftCard nft={givenAnNft(givenAnNft({ title: 'nft title' }))} />)
+    Render(<NftCard nft={givenAnNft({ title: 'nft title' })} />)
     expect(screen.getByText(/nft title/i)).toBeInTheDocument()
   })
 })
-
-function givenAnNft(nft: Partial<NFT>): NFT {
-  return {
-    title: nft.title ?? 'irrelevant',
-    name: nft.name ?? 'irrelevant',
-    description: nft.description ?? 'irrelevant',
-    address: nft.address ?? 'irrelevant',
-    fav: false,
-    imageUrl: nft.imageUrl ?? 'irrelevant',
-    details: [],
-    attributes: [],
-
-    ...nft
-  }
-}
