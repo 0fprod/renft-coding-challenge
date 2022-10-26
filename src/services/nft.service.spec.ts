1db81112a1306458ab1b9294ad754bc804a3f8c2
franjpr <franp.dev@protonmail.com>
2022-10-26 16:30:21 +0100

    refactor: merge repositories result

---
+++ src/services/nft.service.spec.ts
@@ -0,0 +1,39 @@
+import { createNftService } from './nft.service'
+import { createNftEndpoint } from '../repositories/api/nft.endpoint'
+import { createAzraelContractIndexer } from '../repositories/graphql/azrael/contract.indexer'
+describe('NFT Service', () => {
+  it('merges nft data from repositories', async () => {
+    // Arrange
+    const azraelRepository = createAzraelContractIndexer()
+    const alchemyRepository = createNftEndpoint()
+    vi.spyOn(azraelRepository, 'getLendingNfts').mockResolvedValue([
+      {
+        address: '0xAddress',
+        tokenId: '1'
+      },
+      {
+        address: '0xAddress',
+        tokenId: '2'
+      }
+    ])
+    const alchemySpy = vi.spyOn(alchemyRepository, 'getNftMetadata').mockResolvedValue({
+      address: '0xAddress',
+      name: 'aName',
+      description: 'aDescription',
+      imageUrl: 'anImageUrl',
+      title: 'aTitle',
+      attributes: [],
+      details: []
+    })
+    const service = createNftService(azraelRepository, alchemyRepository)
+    // Act
+    const [nft] = await service.getNFTs()
+    // Assert
+    expect(alchemySpy).toHaveBeenCalledTimes(2)
+    expect(nft.address).toEqual('0xAddress')
+    expect(nft.name).toEqual('aName')
+    expect(nft.title).toBe('aTitle')
+    expect(nft.imageUrl).toEqual('anImageUrl')
+    expect(nft.tokenId).toEqual('1')
+  })
+})
--- src/services/nft.service.ts
+++ src/services/nft.service.ts
@@ -1,17 +1,47 @@
+import { NFT } from '../models/NFT'
 import { NftData } from '../models/NFTData'
 import { NFTMetadata } from '../models/NFTMetadata'
 import { NftEndpoint } from '../repositories/api'
 import { AzraelContractIndexer } from '../repositories/graphql/azrael'
 
 export interface NftService {
-  getLendingNfts: () => Promise<NftData[]>
-  getNftMetadata: (nftAddress: string, tokenId: string) => Promise<NFTMetadata>
+  getNFTs: () => Promise<NFT[]>
 }
 
 export const createNftService = (
-  azralRepository: AzraelContractIndexer,
+  azraelRepository: AzraelContractIndexer,
   nftMetadataRepository: NftEndpoint
-): NftService => ({
-  getLendingNfts: azralRepository.getLendingNfts,
-  getNftMetadata: nftMetadataRepository.getNftMetadata
-})
+): NftService => {
+  const getNFTs = async (): Promise<NFT[]> => {
+    const nftData = await azraelRepository.getLendingNfts()
+    const nftMetadataPromises = nftData.map((item) => nftMetadataRepository.getNftMetadata(item.address, item.tokenId))
+    const nftMetadata = await Promise.allSettled(nftMetadataPromises)
+    const nfts = merge(nftData, nftMetadata)
+    return nfts
+  }
+
+  const merge = (nftData: NftData[], nftMetadata: Array<PromiseSettledResult<NFTMetadata>>): NFT[] => {
+    const nftList: NFT[] = []
+    for (let index = 0; index < nftMetadata.length; index++) {
+      const element = nftMetadata[index]
+      if (element.status === 'rejected') continue
+
+      nftList.push({
+        address: nftData[index].address,
+        tokenId: nftData[index].tokenId,
+        description: element.value.description,
+        fav: false,
+        imageUrl: element.value.imageUrl,
+        name: element.value.name,
+        title: element.value.title,
+        details: [],
+        attributes: []
+      })
+    }
+    return nftList
+  }
+
+  return {
+    getNFTs
+  }
+}
