620a648b10fd311754ae77e0164b1f9ffaafde2b
franjpr <franp.dev@protonmail.com>
2022-10-27 18:22:18 +0100

    fix: add id as key prop

--- src/components/Showcase/Showcase.spec.tsx
+++ src/components/Showcase/Showcase.spec.tsx
@@ -5,18 +5,26 @@ import { Showcase } from './Showcase'
 
 describe('Showcase', () => {
   it('renders one nft', () => {
-    Render(<Showcase nfts={[givenAnNft({ title: 'a title' })]} />)
+    Render(<Showcase nfts={[givenAnNft({ title: 'a title', id: '1' })]} toggleFav={vi.fn()} />)
     expect(screen.getByText(/a title/i)).toBeInTheDocument()
   })
 
   it('renders many nft', () => {
-    const collection = [givenAnNft({ title: 'a title' }), givenAnNft({ title: 'another title' })]
-    Render(<Showcase nfts={collection} />)
+    const collection = [givenAnNft({ title: 'a title', id: '1' }), givenAnNft({ title: 'another title', id: '2' })]
+    Render(<Showcase nfts={collection} toggleFav={vi.fn()} />)
     expect(screen.getByText(/a title/i)).toBeInTheDocument()
     expect(screen.getByText(/another title/i)).toBeInTheDocument()
   })
+
   it('renders a message when there are no nfts', () => {
-    Render(<Showcase nfts={[]} />)
+    Render(<Showcase nfts={[]} toggleFav={vi.fn()} />)
     expect(screen.queryByText(/no nfts/)).toBeInTheDocument()
+  })
+
+  it('emits an event when an nft is favourited', async () => {
+    const spy = vi.fn()
+    const { user } = Render(<Showcase nfts={[givenAnNft({ title: 'a title', id: '1' })]} toggleFav={spy} />)
+    await user.click(screen.getByLabelText('fav'))
+    expect(spy).toBeCalled()
   })
 })
