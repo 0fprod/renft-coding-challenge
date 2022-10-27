eb7e5b0119b1b68cc9f545c1e6749e50fec2a6d2
franjpr <franp.dev@protonmail.com>
2022-10-27 18:22:42 +0100

    feat: implement favourites

--- src/views/ShowcaseView/ShowcaseView.spec.tsx
+++ src/views/ShowcaseView/ShowcaseView.spec.tsx
@@ -1,39 +1,65 @@
 import { act, screen } from '@testing-library/react'
 import { Render } from '../../../tests/render'
-import * as hook from '../../hooks/useNft.hook'
+import { ServiceContext } from '../../contexts/services.context'
 import { ShowcaseView } from './ShowcaseView'
 
 describe('ShowcaseView', () => {
   it('renders', () => {
     Render(<ShowcaseView />)
+    expect(screen.getByText(/showcase view/i)).toBeInTheDocument()
   })
 
   it('fetches data on mount', async () => {
-    const fetchMock = vi.fn().mockResolvedValue([])
-    vi.spyOn(hook, 'useNft').mockImplementationOnce(() => ({
-      getNFTs: fetchMock
-    }))
-    await act(() => {
-      Render(<ShowcaseView />)
+    const spy = vi.fn().mockImplementation(() => {
+      return Promise.resolve([])
     })
 
-    expect(fetchMock).toHaveBeenCalledTimes(1)
+    await act(() => {
+      Render(
+        <ServiceContext.Provider
+          value={{
+            nftService: { getNFTs: spy },
+            storageService: {
+              addFav: vi.fn(),
+              deleteFav: vi.fn(),
+              isFav: vi.fn()
+            }
+          }}
+        >
+          <ShowcaseView />
+        </ServiceContext.Provider>
+      )
+    })
+
+    expect(spy).toHaveBeenCalledTimes(1)
   })
 
   it('filters by title', async () => {
-    vi.spyOn(hook, 'useNft').mockImplementationOnce(() => ({
-      getNFTs: vi.fn().mockResolvedValue([
-        {
-          title: 'a title',
-          tokenId: '1'
-        },
-        {
-          title: 'another title',
-          tokenId: '2'
-        }
-      ])
-    }))
-    const { user } = Render(<ShowcaseView />)
+    const { user } = Render(
+      <ServiceContext.Provider
+        value={{
+          nftService: {
+            getNFTs: vi.fn().mockResolvedValue([
+              {
+                title: 'a title',
+                id: '1'
+              },
+              {
+                title: 'another title',
+                id: '2'
+              }
+            ])
+          },
+          storageService: {
+            addFav: vi.fn(),
+            deleteFav: vi.fn(),
+            isFav: vi.fn()
+          }
+        }}
+      >
+        <ShowcaseView />
+      </ServiceContext.Provider>
+    )
     const search = screen.getByRole('searchbox')
 
     await user.type(search, 'a ti')
--- src/views/ShowcaseView/ShowcaseView.tsx
+++ src/views/ShowcaseView/ShowcaseView.tsx
@@ -1,22 +1,19 @@
-import { useCallback, useEffect, useState } from 'react'
-import { useNft } from '../../hooks/useNft.hook'
+import React, { useEffect, useState } from 'react'
+// import { useNft } from '../../hooks/useNft.hook'
 import { NFT } from '../../models/NFT'
 import { Showcase } from '../../components/Showcase/Showcase'
 import { Filters } from '../../components/Filters/Filters'
+import { useStorage } from '../../hooks/useStorage.hook'
+import { useNft } from '../../hooks/useNft.hook'
+
 const ITEMS_PER_PAGE = 2
 
 export const ShowcaseView: React.FC<{}> = () => {
   const [allNfts, setNfts] = useState<NFT[]>([])
   const [filteredNfts, setFilteredNfts] = useState<NFT[]>([])
   const { getNFTs } = useNft()
+  const { addFav, deleteFav, isFav } = useStorage()
   const [page, setPage] = useState(0)
-
-  const fetch = useCallback(() => {
-    getNFTs(ITEMS_PER_PAGE, page).then((nfts) => {
-      setNfts([...allNfts, ...nfts])
-      setFilteredNfts([...allNfts, ...nfts])
-    })
-  }, [page])
 
   const fetchMore = (): void => {
     setPage(page + 1)
@@ -26,8 +23,24 @@ export const ShowcaseView: React.FC<{}> = () => {
     setFilteredNfts(allNfts.filter((item) => item.title.toLowerCase().includes(term.toLowerCase())))
   }
 
+  const toggleFav = (id: string): void => {
+    const index = allNfts.findIndex((i) => i.id === id)
+    allNfts[index].fav = !allNfts[index].fav
+    setNfts([...allNfts])
+    allNfts[index].fav ? addFav(id) : deleteFav(id)
+  }
+
   useEffect(() => {
-    fetch()
+    setFilteredNfts(allNfts)
+  }, [allNfts])
+
+  useEffect(() => {
+    getNFTs(ITEMS_PER_PAGE, page).then((nfts) => {
+      nfts.forEach((item) => {
+        item.fav = isFav(item.id)
+      })
+      setNfts([...allNfts, ...nfts])
+    })
   }, [page])
 
   return (
@@ -36,7 +49,7 @@ export const ShowcaseView: React.FC<{}> = () => {
       <button onClick={fetchMore}> Fetch More</button>
       <Filters onInput={filterHandler} toggleAvailable={() => {}} toggleFavourites={() => {}} />
       <br />
-      <Showcase nfts={filteredNfts} />
+      <Showcase nfts={filteredNfts} toggleFav={toggleFav} />
     </div>
   )
 }
