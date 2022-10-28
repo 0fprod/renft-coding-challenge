d8dd828fee0ba4e9f0de4b38845c6e4e6caceee2
franjpr <franp.dev@protonmail.com>
2022-10-28 08:28:30 +0100

    refactor: moved favstate to service

--- src/hooks/useNft.hook.ts
+++ src/hooks/useNft.hook.ts
@@ -1,11 +1,22 @@
 import React from 'react'
 import { ServiceContext } from '../contexts/services.context'
+import { NFT } from '../models/NFT'
 import { NftService } from '../services/nft.service'
 
 export const useNft = (): NftService => {
-  const { nftService } = React.useContext(ServiceContext)
+  const { nftService, storageService } = React.useContext(ServiceContext)
+  const { isFav } = storageService
+
+  const getNFTsWithFavState = (perPage: number, page?: number): Promise<NFT[]> => {
+    return nftService.getNFTs(perPage, page).then((nfts) =>
+      nfts.map((nft) => {
+        nft.fav = isFav(nft.id)
+        return nft
+      })
+    )
+  }
 
   return {
-    ...nftService
+    getNFTs: getNFTsWithFavState
   }
 }
