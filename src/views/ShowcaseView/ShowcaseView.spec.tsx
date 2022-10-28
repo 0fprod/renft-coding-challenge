23542051fb322dfa0065da4f71afa02470c00925
franjpr <franp.dev@protonmail.com>
2022-10-29 00:43:39 +0100

    fix: update test

--- src/views/ShowcaseView/ShowcaseView.spec.tsx
+++ src/views/ShowcaseView/ShowcaseView.spec.tsx
@@ -62,7 +62,7 @@ describe('ShowcaseView', () => {
         fav: false
       })
     ]
-    const { user, debug } = Render(
+    const { user } = Render(
       wrapShowcaseViewWithContext({
         getNFTs: vi.fn().mockResolvedValue(mockedNfts),
         isFav(id) {
@@ -73,7 +73,6 @@ describe('ShowcaseView', () => {
 
     const checkbox = screen.getByRole('checkbox', { name: 'filter availables only' })
     await user.click(checkbox)
-    debug()
 
     expect(screen.getByText(/a title/i)).toBeInTheDocument()
     expect(screen.queryByText(/another title/i)).not.toBeInTheDocument()
@@ -85,7 +84,10 @@ function wrapShowcaseViewWithContext(services: Partial<Services>): any {
   return (
     <ServiceContext.Provider
       value={{
-        nftService: { getNFTs: services.getNFTs ?? vi.fn().mockResolvedValue([]) },
+        nftService: {
+          getNFTs: services.getNFTs ?? vi.fn().mockResolvedValue([]),
+          getAvailableNFTs: services.getAvailableNFTs ?? vi.fn().mockResolvedValue([])
+        },
         storageService: {
           addFav: services.addFav ?? vi.fn,
           deleteFav: services.deleteFav ?? vi.fn,
